a3a3d457bcaaeb0b82dbf950f99befd28377dcad
Annen0503 <36098988+Annen0503@users.noreply.github.com>
2018-04-22 17:47:10 +0900

    MVVMパターン学習01
    四則計算アプリ

---
+++ Sample01/MVVMCalc/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MVVMCalc
+{
+    using MVVMCalc.View;
+    using System.Windows;
+
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var v = new MainView();
+            v.Show();
+        }
+    }
+}
