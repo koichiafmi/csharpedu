eb0064f286e0323b29e25ced69e5bd78db006a8b
hide_satou <satou9h@yahoo.co.jp>
2018-05-25 23:35:58 +0900

    XML読込追加　/　CSVヘッダー情報を列名に表示

--- WindowsFormsAppSample001/WindowsFormsAppSample002/Form1.Designer.cs
+++ WindowsFormsAppSample001/WindowsFormsAppSample002/Form1.Designer.cs
@@ -37,17 +37,27 @@
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(7, 20);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 46);
             this.button1.TabIndex = 0;
@@ -66,11 +76,11 @@
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 122);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 211);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(752, 302);
+            this.dataGridView1.Size = new System.Drawing.Size(752, 213);
             this.dataGridView1.TabIndex = 1;
             // 
             // Column1
@@ -105,14 +115,15 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(425, 12);
+            this.textBox1.Location = new System.Drawing.Point(425, 64);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(339, 22);
+            this.textBox1.Size = new System.Drawing.Size(339, 42);
             this.textBox1.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 12);
+            this.button2.Location = new System.Drawing.Point(122, 21);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(102, 46);
             this.button2.TabIndex = 3;
@@ -120,29 +131,15 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(425, 41);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(339, 22);
-            this.textBox2.TabIndex = 4;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(265, 12);
+            this.button3.Location = new System.Drawing.Point(240, 21);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 46);
             this.button3.TabIndex = 5;
             this.button3.Text = "CSV読込3\n(複列：5)";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(425, 70);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(339, 22);
-            this.textBox3.TabIndex = 6;
             // 
             // menuStrip1
             // 
@@ -155,10 +152,111 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(12, 68);
+            this.textBox4.Location = new System.Drawing.Point(425, 30);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(354, 22);
+            this.textBox4.Size = new System.Drawing.Size(339, 22);
             this.textBox4.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 76);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CSV読込比較";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 42);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "CSV\r\n読込";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(103, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(121, 34);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "1行目\r\n列名として表示";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Location = new System.Drawing.Point(19, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(344, 80);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "CSV";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(15, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 53);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "XML\r\n読込";
+            this.button5.Enabled = false;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Location = new System.Drawing.Point(425, 114);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(339, 80);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "XML";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(174, 21);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 52);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "列作成";
+            this.button7.Enabled = false;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(95, 21);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 53);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "列削除";
+            this.button6.Enabled = false;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(238, 34);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 19);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "CSV読込";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // Form1
             // 
@@ -166,18 +264,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Sample002";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,11 +294,18 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- WindowsFormsAppSample001/WindowsFormsAppSample002/Form1.cs
+++ WindowsFormsAppSample001/WindowsFormsAppSample002/Form1.cs
@@ -9,18 +9,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.VisualBasic.FileIO;
+using System.Xml;
 
 namespace WindowsFormsAppSample002
 {
     public partial class Form1 : Form
     {
-        private String READ_FILE_USED_IN_VISUALBASIC_FILEIO = @"C:\Sample002.csv";
-        private String READ_FILE_USED_IN_FILEIO = @"C:\Sample002_5column10000rows.csv";
+        private String READ_FILE_USED_IN_VISUALBASIC_FILEIO = @"C:\Sample001\Sample002.csv";
+        private String READ_FILE_USED_IN_FILEIO = @"C:\Sample001\Sample002_5column10000rows.csv";
         //private String READ_FILE_USED_IN_FILEIO  = @"C:\Sample002_5column100rows.csv";
 
         private int READ_CSV_TEXTFIELDPARSER = 2; // Microsoft.VisualBasic.FileIO : dataGridViewに一行ずつADD
         private int READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ONELINE = 0; // System.IO : dataGridViewに一行ずつADD
         private int READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ALLLINE = 1; // System.IO : dataGridViewに一括ADD
+
+        private String READ_CSV_PERSONAL_INFOMATION = @"C:\Sample001\personal_infomation.csv";
+        private String READ_XML_PERSONAL_INFOMATION = @"C:\Sample001\personal_infomation.xml";
+
+        private int DEFAULT_DISPLAY = 1;
+        private int FIRSTLINE_OF_CSV = 2;
+
+
+        private int FILE_TYPE_CSV = 0;
+        private int FILE_TYPE_XML = 1;
 
 
         public Form1()
@@ -30,17 +41,17 @@ namespace WindowsFormsAppSample002
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.Enabled = false;
             // データをすべてクリア
-            dataGridView1.Rows.Clear();
-            this.TextBox_Clear();
+            this.Initial_Preparation();
             this.Set_DataGridView_Columns_Visible(READ_CSV_TEXTFIELDPARSER);
             this.DataLoad(READ_CSV_TEXTFIELDPARSER);
+            this.Enabled = true;
         }
         private void button2_Click(object sender, EventArgs e)
         {
             // データをすべてクリア
-            dataGridView1.Rows.Clear();
-            this.TextBox_Clear();
+            this.Initial_Preparation();
             this.Set_DataGridView_Columns_Visible(READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ONELINE);
             this.DataLoad(READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ONELINE);
         }
@@ -48,11 +59,34 @@ namespace WindowsFormsAppSample002
         private void button3_Click(object sender, EventArgs e)
         {
             // データをすべてクリア
-            dataGridView1.Rows.Clear();
-            this.TextBox_Clear();
+            this.Initial_Preparation();
             this.Set_DataGridView_Columns_Visible(READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ALLLINE);
             this.DataLoad(READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ALLLINE);
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            // データをすべてクリア
+            this.Initial_Preparation();
+            this.Set_DataGridView_Columns_Visible(READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ALLLINE);
+            //CSV読込表示
+            this.DependsOnFileTypeDataLoad(FILE_TYPE_CSV);
+            this.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            dataGridView1.DataSource = null; // 不要かも
+            dataGridView1.Columns.Clear(); ;
+            dataGridView1.Refresh();
+            this.TextBox_Clear();
+            //XML読込表示
+            this.DependsOnFileTypeDataLoad(FILE_TYPE_XML);
+            this.Enabled = true;
+        }
+
 
         private void DataLoad(int method)
         {
@@ -79,8 +113,8 @@ namespace WindowsFormsAppSample002
                     //ストップウォッチを止める
                     sw.Stop();
                     //結果を表示する
-                    textBox2.Text = string.Concat("CSV読込2-経過時間[", rowCount, "行][ ", sw.Elapsed.Hours.ToString("00"), ":", sw.Elapsed.Minutes.ToString("00"), ":", sw.Elapsed.Seconds.ToString("00"), ":", sw.ElapsedMilliseconds.ToString("000"), " ]");
-                    textBox4.Text = "System.IOクラス使用 : dataGridViewに一行ずつADD";
+                    textBox4.Text = string.Concat("CSV読込2-経過時間[", rowCount, "行][ ", sw.Elapsed.Hours.ToString("00"), ":", sw.Elapsed.Minutes.ToString("00"), ":", sw.Elapsed.Seconds.ToString("00"), ":", sw.ElapsedMilliseconds.ToString("000"), " ]");
+                    textBox1.Text = "System.IOクラス使用\r\ndataGridViewに一行ずつADD";
                 }
                 else if (READ_CSV_FILE_READLINES_DATAGRIDVIEW_ROWS_ADD_ALLLINE == method)
                 {
@@ -109,8 +143,8 @@ namespace WindowsFormsAppSample002
                     //ストップウォッチを止める
                     sw.Stop();
                     //結果を表示する
-                    textBox3.Text = string.Concat("CSV読込3-経過時間[", rowCount, "行][ ", sw.Elapsed.Hours.ToString("00"), ":", sw.Elapsed.Minutes.ToString("00"), ":", sw.Elapsed.Seconds.ToString("00"), ":", sw.ElapsedMilliseconds.ToString("000"), " ]");
-                    textBox4.Text = "System.IOクラス使用 : dataGridViewに一括ADD";
+                    textBox4.Text = string.Concat("CSV読込3-経過時間[", rowCount, "行][ ", sw.Elapsed.Hours.ToString("00"), ":", sw.Elapsed.Minutes.ToString("00"), ":", sw.Elapsed.Seconds.ToString("00"), ":", sw.ElapsedMilliseconds.ToString("000"), " ]");
+                    textBox1.Text = "System.IOクラス使用\r\ndataGridViewに一括ADD";
                 }
                 else if (READ_CSV_TEXTFIELDPARSER == method)
                 {
@@ -130,8 +164,9 @@ namespace WindowsFormsAppSample002
                     //ストップウォッチを止める
                     sw.Stop();
                     //結果を表示する
-                    textBox1.Text = string.Concat("CSV読込1-経過時間[", rowCount, "行][ ", sw.Elapsed.Hours.ToString("00"), ":", sw.Elapsed.Minutes.ToString("00"), ":", sw.Elapsed.Seconds.ToString("00"), ":", sw.ElapsedMilliseconds.ToString("000"), " ]");
-                    textBox4.Text = "TextFieldParserクラスを使用したCSV読込";                }
+                    textBox4.Text = string.Concat("CSV読込1-経過時間[", rowCount, "行][ ", sw.Elapsed.Hours.ToString("00"), ":", sw.Elapsed.Minutes.ToString("00"), ":", sw.Elapsed.Seconds.ToString("00"), ":", sw.ElapsedMilliseconds.ToString("000"), " ]");
+                    textBox1.Text = "TextFieldParserクラスを使用したCSV読込";
+                }
             }
             catch (System.IO.FileNotFoundException)
             {
@@ -157,13 +192,108 @@ namespace WindowsFormsAppSample002
                 return;
             }
         }
+        private void DependsOnFileTypeDataLoad(int fileType)
+        {
+            int rowCount = 0;
+            string filename = "";
 
+            try
+            {
+                filename = READ_CSV_PERSONAL_INFOMATION;
+                string[] csvDataAll = File.ReadAllLines(filename, Encoding.UTF32);
+                DataGridViewRow[] rows;
+                //データの分配列を用意
+                if (checkBox1.Checked == false)
+                {
+                    rows = new DataGridViewRow[csvDataAll.Length];
+                }
+                else
+                {
+                    rows = new DataGridViewRow[csvDataAll.Length - 1];
+                }
+
+                if (FILE_TYPE_CSV == fileType)
+                {
+                    int rowCounta = 0;
+                    //CSVファイルの読み込み
+                    for (int i = 0; i < csvDataAll.Length; i++)
+                    {
+                        rowCounta = i;
+                        string[] csv = csvDataAll[i].Split(',');
+                        string[] data = new string[5];
+                        Array.Copy(csv, 0, data, 0, 5);
+                        if (checkBox1.Checked == true && i == 0)
+                        {
+                            // checkBox1がチェックされている場合
+                            Set_ColumnName(FIRSTLINE_OF_CSV, data);
+                        }
+                        else
+                        {
+                            if (checkBox1.Checked == true)
+                            {
+                                rowCounta--;
+                            }
+                            DataGridViewRow row = new DataGridViewRow();
+                            row.CreateCells(this.dataGridView1);
+                            row.SetValues(data);
+                            rows[rowCounta] = row;
+                            rowCount++;
+                        }
+                    }
+                    if (checkBox1.Checked == false)
+                    {
+                        textBox4.Text = string.Concat("CSV読込");
+                        textBox1.Text = "先頭行もデータとして表示";
+                    } else
+                    {
+                        textBox4.Text = string.Concat("CSV読込");
+                        textBox1.Text = "先頭行は列名として表示";
+                    }
+                    this.dataGridView1.Rows.AddRange(rows);
+                }
+                else if (FILE_TYPE_XML == fileType)
+                {
+                    filename = READ_XML_PERSONAL_INFOMATION;
+                    //　XMLの読み込み
+                    XmlTextReader xmlDataReader = new XmlTextReader(filename);
+
+                    DataSet ds = new DataSet();
+                    ds.ReadXml(xmlDataReader);
+
+                    this.dataGridView1.DataSource = ds.Tables[0];
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                string message = string.Concat("読込むファイルが見つかりません！\n", "ファイル名：", filename);
+                MessageBox.Show(message, "dialog");
+                return;
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                string message = string.Concat("読込むファイルが見つかりません！\n", "ファイル名：", filename);
+                MessageBox.Show(message, "dialog");
+                return;
+
+            }
+        }
+
+        private void Initial_Preparation()
+        {
+            //初期準備(データのクリア等)
+            dataGridView1.DataSource = null; // 不要かも
+            dataGridView1.Rows.Clear();
+            Set_ColumnName(DEFAULT_DISPLAY, null);
+            TextBox_Clear();
+            dataGridView1.Refresh();
+
+        }
         private void TextBox_Clear()
         {
             textBox1.Text = "";
-            textBox2.Text = "";
-            textBox3.Text = "";
             textBox4.Text = "";
+            textBox1.Refresh();
+            textBox4.Refresh();
         }
 
         private void Set_DataGridView_Columns_Visible(int method)
@@ -195,7 +325,105 @@ namespace WindowsFormsAppSample002
                 dataGridView1.Columns[3].Visible = false;
                 dataGridView1.Columns[4].Visible = false;
             }
+            dataGridView1.Refresh();
+        }
 
+        private void Set_ColumnName(int ColumnNameSettingFlag, string[] columnName)
+        {
+            // DefaultDisplay(ex:Column1):0
+            // FirstLine of CSV:1
+            if (DEFAULT_DISPLAY == ColumnNameSettingFlag)
+            {
+                int columnNo = 1;
+                for (int i=0; i < 5; i++)
+                {
+                    dataGridView1.Columns[i].HeaderText = string.Concat("Column", columnNo);
+                    columnNo++;
+                }
+                //this.Column1.HeaderText = "Column1";
+                //this.Column2.HeaderText = "Column2";
+                //this.Column3.HeaderText = "Column3";
+                //this.Column4.HeaderText = "Column4";
+                //this.Column5.HeaderText = "Column5";
+            }
+            else if (FIRSTLINE_OF_CSV == ColumnNameSettingFlag) 
+            {
+                int columnIndex = 0;
+                foreach (string item in columnName)
+                {
+                    //itemをつかう
+                    dataGridView1.Columns[columnIndex].HeaderText = item;
+                    columnIndex++;
+                }
+            }
+            dataGridView1.Refresh();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                this.button1.Enabled = true;
+                this.button2.Enabled = true;
+                this.button3.Enabled = true;
+                this.button4.Enabled = true;
+                this.checkBox1.Enabled = true;
+                this.button5.Enabled = false;
+                this.button6.Enabled = false;
+                this.button7.Enabled = false;
+
+            }
+            else
+            {
+                this.button1.Enabled = false;
+                this.button2.Enabled = false;
+                this.button3.Enabled = false;
+                this.button4.Enabled = false;
+                this.checkBox1.Enabled = false;
+                this.button5.Enabled = true;
+                this.button6.Enabled = true;
+                this.button7.Enabled = true;
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null; // 不要かも
+            dataGridView1.Columns.Clear();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+ 
+            for (int i=0; i < 5 ; i++)
+            {
+                // 1.DataGridViewColumnオブジェクトを生成
+                DataGridViewTextBoxColumn textColumn = new DataGridViewTextBoxColumn();
+
+                // 2.セルに表示するデータソースの指定(DataRowの列名や、独自クラスのプロパティ名を指定)
+                textColumn.DataPropertyName = string.Concat("col1");
+
+                // 3.ヘッダーのテキスト
+                textColumn.HeaderText = string.Concat("Column", i + 1);
+
+                // 4.列の表示／非表示
+                textColumn.Visible = true;
+
+                // 5.列幅
+                textColumn.Width = 100;
+
+                // 6.読み取り専用
+                textColumn.ReadOnly = true;
+
+                // 7.ユーザーによる列幅の変更可否
+                textColumn.Resizable = DataGridViewTriState.True;
+
+                // 8.列を追加
+                this.dataGridView1.Columns.Add(textColumn);
+
+            }
         }
     }
 }
